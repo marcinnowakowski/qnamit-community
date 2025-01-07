9ec7fad09f8fb804e3565de5ab709dd23b5507ac
Marcin Nowakowski <game.of.no@gmail.com>
2025-01-07 01:07:03 +0100

    QNAMIT-1 1) Patient detail url fixed, 2) CORS turned on.

--- qna_rest/urls.py
+++ qna_rest/urls.py
@@ -3,5 +3,5 @@ from .views import PatientDetailAPIView, PatientRegisterAPIView
 
 urlpatterns = [
     path('patient/', PatientRegisterAPIView.as_view(), name='patient-create-if-doesnt-exist'),
-    path('api/patient/<str:patient_number>/', PatientDetailAPIView.as_view(), name='patient-detail'),
+    path('patient/<str:patient_number>/', PatientDetailAPIView.as_view(), name='patient-detail'),
 ]
